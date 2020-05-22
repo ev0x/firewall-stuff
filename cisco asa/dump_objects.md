53fd1abb754b6684db85a57cd1aebeed0bd3db80
ev0x <5706118+ev0x@users.noreply.github.com>
2020-05-22 16:08:07 +0930

    Updated with dump_objects.py Signed-off-by: ev0x <5706118+ev0x@users.noreply.github.com>

---
+++ cisco asa/dump_objects.md
@@ -0,0 +1,9 @@
+# dump_objects.py
+- create a routes.csv file
+- format should be src_route, dest_route
+- dump your config into config.txt
+- create a ipam_extract.csv file
+- format should be ip,desc
+- create a ipam_subnets.csv
+- format should be subnet,desc
+- run the script dump_objects.py
---
+++ cisco asa/dump_objects.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+import string
+import re
+import mmap
+import csv
+import ipaddress
+
+with open('config.txt', 'r') as f:
+    data = f.read()
+
+o_name = re.findall('name\s[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\s.+', data)
+o_service = re.findall('object\sservice\s.+\n(?:\s.+\n?)+', data)
+o_network = re.findall('object\snetwork\s.+\n(?:\s.+\n?)+', data)
+og_service = re.findall('object-group\sservice\s.+\n(?:\s.+\n?)+', data)
+og_network = re.findall('object-group\snetwork\s.+\n(?:\s.+\n?)+', data)
+hostname = re.findall('hostname\s.+', data)
+print("Firewall Host: " + hostname[0].split(' ')[1])
+print("names: " + str(len(o_name)))
+print("object service: " + str(len(o_service)))
+print("object network: " + str(len(o_network)))
+print("object-group service: " + str(len(og_service)))
+print("object-group network: " + str(len(og_network)))
+
+# pull the ipam ip data into a dict
+with open('ipam_extract.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    data_ipam = {k:v for k, v in reader}
+print("IPAM Entries: " + str(len(data_ipam)))
+
+# pull the ipam subnet data into a dict
+with open('ipam_subnets.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    data_subnets = {k:v for k, v in reader}
+print("IPAM Subnets: " + str(len(data_subnets)))
+
+# pull the routes from the firewall
+with open('routes.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    data_routes = {k:v for k, v in reader}
+print("Routes: " + str(len(data_routes)))
+print("\n")
+
+#object network
+with open('object_network.csv', 'w', newline='') as csvfile:
+    print('Processing... object network')
+    writer = csv.writer(csvfile)
+    objNetwork = {} # create this dict for later
+    lofl = []
+    lofl.append(['Name','Host','Description','Interface'])
+    for i in o_network:
+        clean = list(filter(None, [x.strip() for x in i.split('\n')]))
+        name = clean[0].split(' ')[2]
+        desc = ""
+        try:
+            if re.match('description\s.+', clean[1]):
+                desc = clean[1].split('description ')[1]
+            if re.match('description\s.+', clean[2]):
+                desc = clean[2].split('description ')[1]
+
+        except:
+            pass
+
+        for r in clean:
+            route = "outside" #default route
+            if re.match('host\s.+', r):
+                for k,v in data_routes.items():
+                    host = str(r.split(' ')[1])
+                    if ipaddress.ip_address(host) in ipaddress.ip_network(k):
+                        route = v.split(' ')[0]
+
+                lofl.append([name, r.split(' ')[1], desc, route])
+                objNetwork[name] = r.split(' ')[1]
+
+    writer.writerows(lofl)
+
+#object-group service
+with open('object-group_services.csv', 'w', newline='') as csvfile:
+    print('Processing... object-group services')
+    writer = csv.writer(csvfile)
+    lofl = []
+    lofl.append(['Name', 'Type', 'Service', 'Description', 'Count'])
+    for i in og_service:
+        clean = list(filter(None, [x.strip() for x in i.split('\n')]))
+        name = clean[0].split(' ')[2]
+        try:
+            type = clean[0].split(' ')[3]
+        except:
+            type = "tcp-udp"
+        desc = ""
+        c_port = 0
+        c_range = 0
+        nothing = ""
+        try:
+            if re.match('description\s.+', clean[1]):
+                desc = clean[1].split('description ')[1]
+        except:
+            pass
+
+        for r in clean:
+            if re.match('port-object\seq.+', r): # port-object eq x
+                lofl.append([name, type, r.split(' ')[2], desc, nothing])
+                c_port += 1
+            if re.match('port-object\srange.+', r): # port-object range x x
+                lofl.append([name, type, r.split(' ')[2] + "-" + r.split(' ')[3], desc, nothing])
+                c_range += 1
+
+        for i in range(len(lofl)):
+            if i == 1: pass
+            if lofl[i][0] == name:
+                lofl[i][4] = str(c_port) + ' ports | ' + str(c_range) + ' ranges'
+
+        c_port = 0
+        c_range = 0
+
+    writer.writerows(lofl)
+
+#object-group network
+with open('object-group_network.csv', 'w', newline='') as csvfile:
+    print('Processing... object-group network')
+    writer = csv.writer(csvfile)
+    lofl = []
+    lofl.append(['Name', 'Type', 'Network', 'Interface', 'Description', 'Notes', 'IPAM', 'Subnet'])
+    nothing = ""
+    c_host = 0
+    c_network = 0
+    c_object = 0
+    for i in og_network:
+        clean = list(filter(None, [x.strip() for x in i.split('\n')]))
+        name = clean[0].split(' ')[2]
+        esc = ""
+        try:
+            if re.match('description\s.+', clean[1]):
+                desc = clean[1].split('description ')[1]
+        except:
+            pass
+        for r in clean:
+            ipam = ""
+            subnet = ""
+            route = "outside" #default
+            if re.match('network-object\shost.+', r): # network-object host x
+                if r.split(' ')[2] in data_ipam:
+                    ipam = data_ipam[r.split(' ')[2]]
+                if ipam == "":
+                    # perform a substr dict value lookup
+                    try:
+                        ipam = [k + '|' + v for k,v in data_ipam.items() if r.split(' ')[2].lower() in v.lower()][0]
+                    except:
+                        pass
+
+                for k,v in data_subnets.items():
+                    host = str(r.split(' ')[2])
+                    try:
+                        if ipaddress.ip_address(host) in ipaddress.ip_network(k):
+                            subnet = v
+                    except ValueError:
+                        pass
+                #find the route
+                for k,v in data_routes.items():
+                    host = str(r.split(' ')[2])
+                    try:
+                        if ipaddress.ip_address(host) in ipaddress.ip_network(k):
+                            route = v.split(' ')[0]
+                    except ValueError:
+                        pass
+
+                lofl.append([name, 'host', r.split(' ')[2], route, desc, nothing, ipam, subnet])
+                c_host += 1
+            if re.match('network-object\s[0-9\.\s]+', r): # network-object x.x.x.x x.x.x.x
+                try:
+                    net = ipaddress.ip_network(r.split(' ')[1] + '/' + r.split(' ')[2], strict=False)
+                except ValueError:
+                    pass
+                if net in data_subnets:
+                    ipam = data_subnets[net]
+
+                # find the route
+                for k,v in data_routes.items():
+                    host = str(r.split(' ')[1])
+                    if ipaddress.ip_address(host) in ipaddress.ip_network(k):
+                        route = v.split(' ')[0]
+
+                lofl.append([name, 'network', net, route, desc, nothing, ipam])
+                c_network += 1
+            if re.match('network-object\sobject.+', r): # network-object object x
+                addr = ""
+                if r.split(' ')[2] in objNetwork:
+                    addr = objNetwork[r.split(' ')[2]]
+
+                if addr in data_ipam:
+                    ipam = data_ipam[addr]
+
+                # find the route
+                for k,v in data_routes.items():
+                    host = str(addr)
+                    try:
+                        if ipaddress.ip_address(host) in ipaddress.ip_network(k):
+                            route = v.split(' ')[0]
+                    except:
+                        pass
+
+                lofl.append([name, 'object', r.split(' ')[2] + ' [' + addr + ']', route, desc, nothing, ipam])
+                c_object += 1
+            if re.match('group-object\s.+', r): # group-object ABC-123
+                lofl.append([name, 'group-object', r.split(' ')[1], route, desc, nothing, ipam])
+
+        for i in range(len(lofl)):
+            if i == 1: pass
+            if lofl[i][0] == name:
+                lofl[i][5] = str(c_host) + ' hosts | ' + str(c_network) + ' networks | ' + str(c_object) + ' objects'
+
+        c_host = 0
+        c_network = 0
+        c_object = 0
+
+    writer.writerows(lofl)
